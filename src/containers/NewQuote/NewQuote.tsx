39f5691d524857104b9f5bcc82f61cc368be2fba
Akylbek <hsplay74@gmail.com>
2024-10-26 17:27:59 +0600

    form submit

--- src/containers/NewQuote/NewQuote.tsx
+++ src/containers/NewQuote/NewQuote.tsx
@@ -1,5 +1,8 @@
-import {useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {Quote} from "../../types.ts";
+import axiosApi from "../../axiosApi.ts";
+import {useNavigate, useParams} from "react-router-dom";
+import Loader from "../../components/Loader/Loader.tsx";
 
 const quotesList = [
     {title: 'Star Wars', id: 'star-wars'},
@@ -10,6 +13,10 @@ const quotesList = [
 ];
 
 const NewQuote = () => {
+
+    const {id: params} = useParams();
+    const [spinner, setSpinner] = useState(false);
+    const navigate = useNavigate();
 
     const [quote, setQuote] = useState<Quote>({
         category: '',
@@ -24,11 +31,40 @@ const NewQuote = () => {
       }));
     };
 
-    console.log(quote);
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (params) {
+            await axiosApi.put(`quotes/${params}.json`, quote);
+            navigate('/')
+        } else {
+            setSpinner(true);
+            await axiosApi.post('quotes.json', quote);
+            setSpinner(false);
+            navigate('/');
+        }
+    };
 
+    const getQuote = useCallback(async () => {
+            try {
+                const {data} = await axiosApi.get<Quote>(`quotes/${params}.json`);
+                setQuote(data);
+            } catch (e) {
+                console.log(e);
+            }
+    }, [])
+
+    if (params) {
+        useEffect(() => {
+            getQuote();
+        }, []);
+    }
+
+    if (spinner) {
+        return <Loader />
+    }
 
     return (
-        <form className="d-flex flex-column gap-2 card p-3 w-50 mx-auto">
+        <form className="d-flex flex-column gap-2 card p-3 w-50 mx-auto" onSubmit={onSubmit}>
             <div>
                 <label className="mb-1">Category</label>
                 <select name="category" id="category" className="form-control" onChange={handleChange} value={quote.category} required>
@@ -44,7 +80,7 @@ const NewQuote = () => {
             </div>
             <div className="d-flex flex-column">
                 <label>Quote</label>
-                <textarea name="description" id="description" style={{height: 300}} onChange={handleChange} value={quote.description}></textarea>
+                <textarea name="description" id="description" style={{height: 300, padding: 5}} onChange={handleChange} value={quote.description}></textarea>
             </div>
             <button className="btn btn-outline-primary">Save</button>
         </form>
